219a94c55780a90b22e8b3d58040899ce5cd0d6a
Alex Delgado <gadc1996@gmail.com>
2021-11-24 13:11:54 -0600

    feat(): build for production

---
+++ public/precache-manifest.80d397db0fb529feaab47c8c9706a098.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8a739b1a153bbe1c981",
+    "url": "/css/app.98dbaeea.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "1a018c8afd13682cbcc07354062c7ce8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "684b04375a8de7e3838d",
+    "url": "/js/about.567ce7a6.js"
+  },
+  {
+    "revision": "e8a739b1a153bbe1c981",
+    "url": "/js/app.08231800.js"
+  },
+  {
+    "revision": "daae1ae07a295e387c63",
+    "url": "/js/chunk-vendors.a210eb94.js"
+  },
+  {
+    "revision": "4b14c64efaf846819b9a229b4193c8b7",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
